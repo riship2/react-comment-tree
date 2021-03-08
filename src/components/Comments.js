01ce80f573b96c308519bf86971bffcfa014df29
riship2 <riship092020@gmail.com>
2021-03-07 21:29:59 -0500

    WIP - making border clickable

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -7,9 +7,15 @@ export const Comments = (props) => {
   return <>
     {
       (comments || []).map((comment, i) => (
-        <div className='comment'>
-          <div>{comment.text}</div>
-          <><Comments comments={comment.childComments} /></>
+        <div className='comment-container'>
+          {/* todo - make border clickable next (for collapsible) */}
+          {/* <a href='#'> */}
+            <div className='comment-link'/>
+          {/* </a> */}
+          <div className='comment-content'>
+            <div>{comment.text}</div>
+            <><Comments comments={comment.childComments} /></>
+          </div>
         </div>
       ))
     }
